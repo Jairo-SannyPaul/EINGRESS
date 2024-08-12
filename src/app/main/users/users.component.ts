347b480f3669f0b471b35f724cfe8c038f82f1be
lorlorenss <l.anasco.513898@umindanao.edu.ph>
2024-08-12 10:03:06 +0800

    dynamic placeholder based on selectedFilter

--- src/app/main/users/users.component.ts
+++ src/app/main/users/users.component.ts
@@ -10,11 +10,25 @@ import { AddUserFormComponent } from './add-user-form/add-user-form.component';
 export class UsersComponent {
   @ViewChild(AddUserFormComponent) addUserFormContainer!: AddUserFormComponent;
   filterToggle: boolean = false;
+  selectedFilter: string = 'name';
+
   onAddUserBtnClicked(){
     this.addUserFormContainer.showAddUserForm();
   }
-
   toggleFilter(){
     this.filterToggle = !this.filterToggle;
+  }
+
+  selectName(){
+    this.selectedFilter ='name';
+  }
+  selectRole(){
+    this.selectedFilter ='role';
+  }
+  selectRfid(){
+    this.selectedFilter ='rfid';
+  }
+  selectFingerprint(){
+    this.selectedFilter ='fingerprint';
   }
 }
