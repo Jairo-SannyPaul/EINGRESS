45ddbcf00c3deff6d891af51742666aa28487c16
lorlorenss <l.anasco.513898@umindanao.edu.ph>
2024-08-12 17:12:03 +0800

    animation for filterTab

--- src/app/main/users/table/table.component.ts
+++ src/app/main/users/table/table.component.ts
@@ -2,12 +2,31 @@ import { Component, EventEmitter, Input, ViewChild, } from '@angular/core';
 import { UserSelectionComponent } from './user-selection/user-selection.component';
 import { Employee } from 'src/app/interface/employee.interface';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
-
+import { trigger, state, style, transition, animate } from '@angular/animations';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
+  animations: [
+    trigger('slideIn', [
+      state('void', style({
+        transform: 'translateY(-20px)', /* Start from above */
+        opacity: 0
+      })),
+      state('*', style({
+        transform: 'translateY(0)', /* End at original position */
+        opacity: 1
+      })),
+      transition('void => *', [
+        animate('0.2s ease-out')
+      ]),
+      transition('* => void', [
+        animate('0.2s ease-in')
+      ])
+    ])
+  ]
 })
+
 export class TableComponent  {
   @Input() sortOption: string = '';
   @ViewChild(EmployeeDetailsComponent) employeeDetailsComponent!: EmployeeDetailsComponent; 
