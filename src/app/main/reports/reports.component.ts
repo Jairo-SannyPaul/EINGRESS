9dd09f007c30473f2329ef4cf6639cf23c91c79f
lorlorenss <l.anasco.513898@umindanao.edu.ph>
2024-08-20 11:02:46 +0800

    Added Branch for Searchfield filter in Reports Page

--- src/app/main/reports/reports.component.ts
+++ src/app/main/reports/reports.component.ts
@@ -181,4 +181,10 @@ export class ReportsComponent implements OnInit {
     this.reportsSearchFieldComponent.clearSearchField();
   }
 
+  selectBranch(){
+    this.selectedReportsFilter = 'branch';
+    this.employeeService.setFilterOption(this.selectedReportsFilter);
+    this.reportsSearchFieldComponent.clearSearchField();
+  }
+
 }
