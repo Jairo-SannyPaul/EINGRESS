f663a30cc1ec2db82ded2cbcb6412b367b041df2
Jairo-SannyPaul <sgeraldino@jairosoft.com>
2024-09-18 09:34:04 +0800

    partial dashboard overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,7 +47,6 @@ import { ConfirmDialogComponent } from './dialogs/confirm-dialog/confirm-dialog.
 import { AlertDialogComponent } from './dialogs/alert-dialog/alert-dialog.component';
 import { SuccessDialogComponent } from './dialogs/success-dialog/success-dialog.component';
 import { AdminpopupComponent } from './adminpopup/adminpopup.component';
-import { HeaderNavComponent } from './header-nav/header-nav.component';
 
 
 
@@ -82,7 +81,6 @@ import { HeaderNavComponent } from './header-nav/header-nav.component';
     AlertDialogComponent,
     SuccessDialogComponent,
     AdminpopupComponent,
-    HeaderNavComponent,
   ],
   imports: [
     BrowserModule,
