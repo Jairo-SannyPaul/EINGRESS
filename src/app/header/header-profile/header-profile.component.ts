efb83a288d506479955d1f2748b22cf119e962b2
NPCreno <l.anasco.513898@umindanao.edu.ph>
2024-09-19 18:33:24 +0800

    routes to /admin when clicking the username at the top

--- src/app/header/header-profile/header-profile.component.ts
+++ src/app/header/header-profile/header-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-profile',
@@ -8,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderProfileComponent implements OnInit {
   username: string = '';
 
+  constructor(private router: Router){}
+
   ngOnInit(): void {
     // Retrieve the username from localStorage
     this.username = localStorage.getItem('username') || 'Admin';  // Default to 'Admin' if username is not found
+  }
+
+  toggleActive(event: MouseEvent) {
+    // this.isActive = !this.isActive;
+    // this.isDropdownOpen = this.isActive; 
+    this.router.navigateByUrl('/main/admin')
   }
 }
